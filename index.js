f2173b537df83990a2f1754851ad3a54630cbe8b
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-01-21 11:52:27 +0300

    Big O notation solution

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Time complexity: O(n)
+  Space complexity: O(n)
 */
 
 /* 
-  Add your pseudocode here
+  - iterate over the array of numbers
+  - for the current number, identify a complementary number that adds to our target
+  - (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  - iterate over the remaining numbers in the array
+    - check if any of the remaining numbers is the complement
+      - if so, return true
+- if we reach the end of the array, return false
 */
 
 /*
-  Add written explanation of your solution here
+  
+I need to create a function called hasTargetSum that examines whether 
+there are two numbers in an array whose sum equals a specified target number. 
+For instance, if the array is [1, 2, 3, 4] and the target is 6, the function 
+should return true because 2 and 4 add up to 6. Conversely, if the target is 10, 
+indicating that no pair of numbers in the array sums to 10, the function should return false.
 */
 
 // You can run `node index.js` to view these console logs
